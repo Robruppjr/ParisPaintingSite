5e357a16ea3ac131a35e3068cdbf1457c2c4e6fb
Rob Rupp <robruppjr@gmail.com>
2022-09-25 10:30:59 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ParisPaintingSite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
